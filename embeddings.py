f4fc0725f4173197c33133357579013b66825bd4
Sagar Patil <sagarreddypatil@gmail.com>
2024-01-21 01:20:28 -0500

    WE ARE COOKING

--- embeddings.py
+++ embeddings.py
@@ -1,14 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from PyPDF2 import PdfReader
 from joblib import Memory
+from openai import OpenAI
+import numpy as np
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-mem = Memory("cache")
+mem = Memory("~/.cache")
 
 MODEL_NAME = "text-embedding-ada-002"
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+openai = OpenAI(api_key=OPENAI_API_KEY)
 
 
 @mem.cache
-def pdf_to_text(filename):
+def pdf_to_text(filename: str) -> str:
     reader = PdfReader(filename)
     text = ""
     for page in reader.pages:
@@ -17,21 +27,33 @@ def pdf_to_text(filename):
 
 
 @mem.cache
-def split_text(document):
+def split_text(document: str) -> list[str]:
     # specifically for OpenAI Ada Embeddings
 
     text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        model_name=MODEL_NAME, chunk_size=256, chunk_overlap=0
+        model_name=MODEL_NAME, chunk_size=512, chunk_overlap=0
     )
 
     return text_splitter.split_text(document)
 
+
 @mem.cache
-def embed_chunk(chunk):
-    pass
+def embed_chunks(chunks: list[str]):
+    out = openai.embeddings.create(
+        input=chunks,
+        model=MODEL_NAME,
+    )
+
+    embeddings = []
+    for chunk in out.data:
+        embeddings.append(np.array(chunk.embedding, dtype=np.float32))
+
+    return embeddings
 
 
 text = pdf_to_text("xinu.pdf")
 chunks = split_text(text)
+embeddings = embed_chunks(chunks)
 
-print(len(chunks))
+print(embeddings[0])
+
