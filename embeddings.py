0d755599a7abaa7c7b34a7e7fa9cbbd76b43019d
Sagar Patil <sagarreddypatil@gmail.com>
2024-01-21 01:42:51 -0500

    more code

--- embeddings.py
+++ embeddings.py
@@ -3,11 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from PyPDF2 import PdfReader
+from pypdf import PdfReader
 from joblib import Memory
 from openai import OpenAI
 import numpy as np
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+import string
+PRINTABLE = set(string.printable)
 
 mem = Memory("~/.cache")
 
@@ -23,17 +26,25 @@ def pdf_to_text(filename: str) -> str:
     text = ""
     for page in reader.pages:
         text += page.extract_text()
+
     return text
 
 
 @mem.cache
-def split_text(document: str) -> list[str]:
+def split_material_text(document: str) -> list[str]:
     # specifically for OpenAI Ada Embeddings
 
     text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         model_name=MODEL_NAME, chunk_size=512, chunk_overlap=0
     )
 
+    return text_splitter.split_text(document)
+
+@mem.cache
+def split_exam_text(document: str) -> list[str]:
+    # tailored for exam questions
+
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n"])
     return text_splitter.split_text(document)
 
 
@@ -51,9 +62,12 @@ def embed_chunks(chunks: list[str]):
     return embeddings
 
 
-text = pdf_to_text("xinu.pdf")
-chunks = split_text(text)
-embeddings = embed_chunks(chunks)
+if __name__ == "__main__":
+    textbook = pdf_to_text("xinu.pdf")
+    textbook_embeddings = embed_chunks(split_material_text(textbook))
 
-print(embeddings[0])
+    # exam_questions = pdf_to_text("xinu-midterm-spring23.pdf")
+    # exam_chunks = split_exam_text(exam_questions)
+    # # exam_question_embeddings = embed_chunks(split_material_text(exam_questions))
 
+    # print("\n-----------------\n".join(exam_chunks))
